d3f8b297dc10e750615b181ce39c2c04ac67dde5
Ayush P Gupta <ayushpguptaapg@gmail.com>
2020-12-01 09:28:12 +0530

    Add mime_type utils
    Signed-off-by: Ayush P Gupta <ayushpguptaapg@gmail.com>

--- lib/fa_flutter_core.dart
+++ lib/fa_flutter_core.dart
@@ -5,5 +5,6 @@ export 'src/extensions/behavior_subject.dart';
 export 'src/logger/fa_flutter_logger.dart';
 export 'src/package_info/package_info.dart';
 export 'src/prefs/fa_flutter_prefs.dart';
+export 'src/utils/mime_type.dart';
 export 'src/utils/platform_utils.dart';
 export 'src/utils/utils.dart';
