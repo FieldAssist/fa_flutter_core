524a758be360bd39745b0f7552ff9061e8d45a4b
Ayush P Gupta <ayushpguptaapg@gmail.com>
2021-03-10 10:49:22 +0530

    [Logger] Remove logging code and use fa_dart_logger instead
    Signed-off-by: Ayush P Gupta <ayushpguptaapg@gmail.com>

--- lib/fa_flutter_core.dart
+++ lib/fa_flutter_core.dart
@@ -1,11 +1,12 @@
 library fa_flutter_core;
 
+export 'package:fa_dart_logger/fa_flutter_logger.dart';
+export 'package:rxdart/rxdart.dart';
+
 export 'src/exceptions/my_exception.dart';
 export 'src/extensions/behavior_subject.dart';
-export 'src/logger/fa_flutter_logger.dart';
 export 'src/package_info/package_info.dart';
 export 'src/prefs/fa_flutter_prefs.dart';
 export 'src/utils/mime_type.dart';
 export 'src/utils/platform_utils.dart';
 export 'src/utils/utils.dart';
-export 'package:rxdart/rxdart.dart';
--- lib/src/logger/fa_flutter_logger.dart
+++
@@ -1,4 +0,0 @@
-library fa_flutter_logger;
-
-export 'src/app_log_impl.dart';
-export 'src/base/app_log.dart';
