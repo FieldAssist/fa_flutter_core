baf138d18c19543448b693ae693d68c0a7089e3a
vipin9821 <vishwakarma.vipin0909@gmail.com>
2022-03-04 13:56:27 +0530

    [SharedPrefs] Add mock initial values

--- lib/src/prefs/src/clients/shared_prefs.dart
+++ lib/src/prefs/src/clients/shared_prefs.dart
@@ -7,6 +7,7 @@ class SharedAppPrefs implements AppPrefs {
 
   @override
   Future<void> initialise() async {
+    SharedPreferences.setMockInitialValues({});
     _prefs = await SharedPreferences.getInstance();
   }
 
