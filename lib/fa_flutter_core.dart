786177169cadc1bcfac89de3f768478e1a99b476
Sameer Kumar <sameerkumar.tman@gmail.com>
2022-07-14 16:34:28 +0530

    [Common] Add packages

--- lib/fa_flutter_core.dart
+++ lib/fa_flutter_core.dart
@@ -5,8 +5,11 @@ export 'package:fa_dart_logger/fa_dart_logger.dart';
 export 'package:freezed_annotation/freezed_annotation.dart';
 export 'package:json_annotation/json_annotation.dart';
 export 'package:rxdart/rxdart.dart';
+export 'package:device_info_plus/device_info_plus.dart';
+export 'package:device_information/device_information.dart';
+export 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
+export 'package:package_info_plus/package_info_plus.dart';
 
-export 'src/device_info/device_info.dart';
 export 'src/package_info/package_info.dart';
 export 'src/prefs/fa_flutter_prefs.dart';
 export 'src/result/result.dart';
